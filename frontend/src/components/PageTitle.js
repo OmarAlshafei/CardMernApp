35a97712cd99bc3208914213cb0a3157bbe9c3ee
OmarAlshafei <104457916+OmarAlshafei@users.noreply.github.com>
2023-10-17 22:05:12 -0400

    Update PageTitle.js

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -1,8 +1,9 @@
 import React from 'react';
-function PageTitle()
-{
-return(
-<h1 id="title">COP 4331 MERN Stack Demo</h1>
-);
-};
+
+function PageTitle() {
+  return (
+    <h1 id="title">COP 4331 MERN Stack Demo</h1>
+  );
+}
+
 export default PageTitle;
